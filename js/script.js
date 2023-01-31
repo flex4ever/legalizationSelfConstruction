1c120da29a64751be62d6eb0ccfea24131678e5c
yan4ar <115583304+yan4ar@users.noreply.github.com>
2023-02-01 00:41:22 +0300

    third_block_alfa_2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 let offset = 0;
 const SliderLine = document.querySelector('.slider-line');
 const ImgDecor = document.querySelectorAll('.img-slider-decor');
-let i = 1;
-let k = 0;
+
 document.querySelector('.slider-next').addEventListener('click', function () {
     offset = offset + 262;
     if (offset > 9150) {
@@ -22,46 +21,39 @@ document.querySelector('.slider-prev').addEventListener('click', function () {
 });
 
 
-
-
-document.querySelector('.slider-next').addEventListener('click', function () {
-    ImgDecor[i].classList.add('active');
-    ImgDecor[k].classList.remove('active');
-    if (i >= 0) {
-        i++;
-        if (k < i) {
-            k++;
-        }
+const imgDecor = document.querySelectorAll('.img-slider-decor');
+let i = 1;
+let k = 0;
+const slideNext = document.querySelector('.slider-next')
+const slidePrew = document.querySelector('.slider-prev')
+slideNext.addEventListener('click', ()=>{
+    imgDecor[i].classList.add('active');
+    imgDecor[k].classList.remove('active');
+    i++;
+    k++;
+    console.log(i)
+    console.log(k)
+    if (i == imgDecor.length){
+        i=0;
     }
-    if (i == 35) {
-        i = 0;
-    } if (i == 1) {
-        k = 0;
-
+    if (k == imgDecor.length){
+        k=0;
     }
-});
-document.querySelector('.slider-prev').addEventListener('click', function () {
-    ImgDecor[k].classList.add('active');
-    ImgDecor[i].classList.remove('active');
-    
-    if (i > 0) {
-        i--;
-        k--;
-        console.log(k);
-        console.log(i);
-    } 
-    
-    if (i <= 0) {
-        i = 35;
-        k = 34;
-        console.log(k);
-        console.log(i);
-    } 
-    
-    if (i == 35 && k == 34) {
-        i--;
-        k--;
-        console.log(k);
-        console.log(i);
-    } 
-});
+})
+slidePrew.addEventListener('click', ()=>{
+    i--;
+    k--;
+    imgDecor[i].classList.remove('active');
+    imgDecor[k].classList.add('active');
+    console.log(i)
+    console.log(k)
+    // console.log(imgDecor)
+
+    if (i == 0){
+        i = imgDecor.length;
+    }
+    if (k == 0){
+        k = imgDecor.length;
+    }
+   
+})
